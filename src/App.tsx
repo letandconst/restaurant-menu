935e61a0a011f14b49434ecaa47106c606566ca7
Jm <jmdelasalas.dev@gmail.com>
2024-06-10 17:06:33 +0800

    Updated general files Update the app file for new routes Hide page scrollbar Created base model for the Category and Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LoginForm, RegistrationForm, Dashboard, Items, Categories } from './screens';
 
 import { AuthRedirect, ProtectedRoute } from './utils';
-import { LoginForm, RegistrationForm, Dashboard } from './screens';
+import Layout from './layout/Layout';
 
 function App() {
 	return (
@@ -20,9 +21,22 @@ function App() {
 					</Route>
 					<Route element={<ProtectedRoute />}>
 						<Route
-							path='/'
-							element={<Dashboard />}
-						/>
+							path='/*'
+							element={<Layout />}
+						>
+							<Route
+								index
+								element={<Dashboard />}
+							/>
+							<Route
+								path='items'
+								element={<Items />}
+							/>
+							<Route
+								path='categories'
+								element={<Categories />}
+							/>
+						</Route>
 					</Route>
 				</Routes>
 			</Router>
